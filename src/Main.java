0f95f6a8776c527a899629517223a51bcb116dd3
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-19 10:50:14 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+
+        Collection<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-        List<Integer> secondList = new ArrayList<>();
-        secondList.add(4);
-        secondList.add(5);
-        secondList.add(6);
-        list.addAll(secondList);
-        System.out.println(list);
+        System.out.println(list.add(1));
         System.out.println(list.size());
-
-        for (int i = 0; i < list.size(); i++){
-            System.out.println(i);
-        }
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        List<Integer> secondList = new ArrayList<>();
+//        secondList.add(4);
+//        secondList.add(5);
+//        secondList.add(6);
+//        list.addAll(secondList);
+//        System.out.println(list);
+//        System.out.println(list.size());
+//
+//        for (int i = 0; i < list.size(); i++){
+//            System.out.println(i);
+//        }
     }
 }
