15761ae021bfcc2563496ecd834d1df5ac742011
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-19 10:56:48 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,20 @@ public class Main {
         list.add(3);
         System.out.println(list.add(1));
         System.out.println(list.size());
-//        List<Integer> list = new ArrayList<>();
-//        list.add(1);
-//        list.add(2);
-//        list.add(3);
-//        List<Integer> secondList = new ArrayList<>();
-//        secondList.add(4);
-//        secondList.add(5);
-//        secondList.add(6);
-//        list.addAll(secondList);
-//        System.out.println(list);
+        Collection<Integer> secondList = new ArrayList<>();
+        secondList.add(4);
+        secondList.add(5);
+        secondList.add(6);
+        list.addAll(secondList);
+        System.out.println("Первый лист " + list + " " + "\nРазмер " + list.size() );
+
+//        list.clear();
 //        System.out.println(list.size());
-//
-//        for (int i = 0; i < list.size(); i++){
-//            System.out.println(i);
-//        }
+//        System.out.println(list.isEmpty());
+        Object[]arr = list.toArray();
+
+        for (Object o : arr) {
+            System.out.println(o); // ого
+        }
     }
 }
