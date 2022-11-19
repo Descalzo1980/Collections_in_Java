fbd70428a089f16077e2fe30c314955bb6f27e5a
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-19 16:23:05 +0300

    1

---
+++ src/SimpleMap.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleMap {
+    public static void main(String[] args) {
+
+        int[] marks = {14,42,93,54,55,6,69,7};
+        String[] students = {"Alex", "Bob", "Oak", "Ele"};
+
+        Arrays.sort(marks);
+        Arrays.sort(students);
+
+        for(int i : marks){
+            System.out.println(i);
+        }
+
+        for(String i : students){
+            System.out.println(i);
+        }
+
+//        Map<String,Integer> map = new HashMap<>();
+//        map.put("World", 100);
+//        System.out.println(map.size());
+//        System.out.println(map);
+//        map.put("World1", 150);
+//        System.out.println(map.size());
+//        System.out.println(map);
+//        map.put("World2", 200);
+//        System.out.println(map.size());
+//        System.out.println(map);
+//        map.put("World3", 300);
+//        System.out.println(map.size());
+//        System.out.println(map);
+//
+//        for(Map.Entry<String,Integer> i : map.entrySet()){
+//            System.out.println(i.getKey() + " " + i.getValue());
+//        }
+
+    }
+}
