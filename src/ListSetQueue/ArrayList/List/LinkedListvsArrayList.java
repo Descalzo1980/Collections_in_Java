c2ab5c781458381f16cc2ab364278dc76f042440
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-20 19:13:51 +0300

    1

--- src/ListSetQueue/ArrayList/List/LinkedListvsArrayList.java
+++ src/ListSetQueue/ArrayList/List/LinkedListvsArrayList.java
@@ -2,7 +2,6 @@ package ListSetQueue.ArrayList.List;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 public class LinkedListvsArrayList implements Runnable {
     public static void main(String[] args) {
