6e6d8589fc9021a5d68241307ffd2bd7e05d8adf
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-11-20 21:07:57 +0300

    add

---
+++ src/ListSetQueue/ArrayList/ArrayDeque/Main.java
@@ -0,0 +1,29 @@
+package ListSetQueue.ArrayList.ArrayDeque;
+
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayDeque<String> states = new ArrayDeque<>();
+        states.add("Россия");
+        states.add("Франция");
+
+        states.push("Великобритания");
+
+        String sFirst = states.getFirst();
+        String sLast = states.getLast();
+
+        while (states.peek() != null){
+            System.out.println(states.pop());
+        }
+        System.out.println("Размер очереди " + states.size());
+
+
+        ArrayDeque<Person> people = new ArrayDeque<Person>();
+        people.addFirst((new Person("Tom")));
+        people.addLast((new Person("Alex")));
+        for (Person p : people){
+            System.out.println(p.getName());
+        }
+    }
+}
---
+++ src/ListSetQueue/ArrayList/ArrayDeque/Person.java
@@ -0,0 +1,13 @@
+package ListSetQueue.ArrayList.ArrayDeque;
+
+public class Person {
+    private String name;
+
+    public Person(String value) {
+        name = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
