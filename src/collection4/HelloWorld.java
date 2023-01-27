df003a9929df1dafb8cb0a94b711911072a68265
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-27 15:23:37 +0300

    add

--- src/collection4/HelloWorld.java
+++ src/collection4/HelloWorld.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class HelloWorld {
     public static void main(String[] args) {
         Collection<String> collection = new ArrayList<>();
-        List<String> list = new ArrayList<>();
-        Set<String> set;
-        Queue<String> queue;
-        Map<String, String> map;
+        ListWork<String> list = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        Map<String, String> map = new HashMap<>();
 
         collection.add("one");
         collection.add("two");
@@ -18,10 +18,33 @@ public class HelloWorld {
             System.out.println(s);
         }
 
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        list.add("d");
+        list.add("zero");
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.get(2);
 
+        set.add("zero");
+        set.add("one");
+        set.add("two");
+        set.add("three");
+
+        queue.add("one");
+        queue.add("two");
+        queue.add("three");
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.offer("four"));
+        for (String s: queue){
+            System.out.println(s);
+        }
+
+        map.put("1","one");
+        map.put("2","two");
+        map.put("3","three");
+        map.put("4","four");
+
+        map.remove("1");
+        map.forEach((k,v) -> System.out.println("Key: " + k + " Value: " + v));
     }
 }
---
+++ src/collection4/ListWork.java
@@ -0,0 +1,9 @@
+package collection4;
+
+import java.util.List;
+
+public class ListWork {
+    public static void main(String[] args) {
+        List<String> list;
+    }
+}
