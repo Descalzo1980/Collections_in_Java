94f9799bf441a9f2b0da7b93e485ce07f94aee48
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-02-01 20:50:40 +0300

    add

--- src/collection4/MyLinkedList.java
+++ src/collection4/MyLinkedList.java
@@ -20,6 +20,41 @@ public class MyLinkedList {
         size++;
     }
 
+    public int get(int index) {
+        int currentIndex = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            if (currentIndex == index) {
+                return temp.getValue();
+            } else {
+                temp = temp.getNext();
+                currentIndex++;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public void remove(int index){
+        if(index == 0){
+            head = head.getNext();
+            size--;
+            return;
+        }
+        int currentIndex = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            if (currentIndex == index - 1) {
+                temp.setNext(temp.getNext().getNext());
+                size--;
+                return;
+            } else {
+                temp = temp.getNext();
+                currentIndex++;
+            }
+        }
+    }
     public String toString(){
         int[] result = new int[size];
         int index = 0;
--- src/collection4/Test.java
+++ src/collection4/Test.java
@@ -1,11 +1,40 @@
 package collection4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList();
-        myLinkedList.add(1);
-        myLinkedList.add(2);
-        myLinkedList.add(3);
-        System.out.println(myLinkedList);
+//        MyLinkedList myLinkedList = new MyLinkedList();
+//        myLinkedList.add(1);
+//        myLinkedList.add(2);
+//        myLinkedList.add(3);
+//        System.out.println(myLinkedList);
+//
+//        System.out.println(myLinkedList.get(2));
+//
+//        myLinkedList.remove(0);
+//
+//        System.out.println(myLinkedList);
+
+        List<Integer> integerList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 10000000;i++){
+            integerList.add(i);
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+
+        long start1 = System.currentTimeMillis();
+        for (int i = 0; i < 10000000;i++) {
+            arrayList.add(i);
+        }
+        long end1 = System.currentTimeMillis();
+        System.out.println(end1 - start1);
     }
 }
