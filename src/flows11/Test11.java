de8194c38a38b7824cdecb6fcf7291faa902406c
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-02-11 13:56:40 +0300

    deadlock

---
+++ src/flows11/Test11.java
@@ -0,0 +1,21 @@
+package flows11;
+
+public class Test11 {
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread();
+
+        Thread thread2 = new Thread();
+    }
+}
+class Runner{
+    public void firstThread(){
+
+    }
+    public void secondThread(){
+
+    }
+    public void finish(){
+
+    }
+}
