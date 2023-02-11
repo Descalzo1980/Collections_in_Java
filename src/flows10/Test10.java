658ac4c5c4a98aa05ca8e193f051799e9aa481ae
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-02-11 11:48:56 +0300

    Semaphore practices

--- src/flows10/Test10.java
+++ src/flows10/Test10.java
@@ -1,19 +1,64 @@
 package flows10;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class Test10 {
     public static void main(String[] args) throws InterruptedException {
-        Semaphore semaphore = new Semaphore(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
 
-        semaphore.acquire();
-        semaphore.acquire();
-        semaphore.acquire();
-        System.out.println("All permits have been acquired");
-        semaphore.acquire();
-        System.out.println("Can't reach here...");
-        semaphore.release();
+        Connection connection = Connection.getConnection();
+        for (int i = 0; i < 200; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        connection.work();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }
+        executorService.shutdown();
 
-        System.out.println(semaphore.availablePermits());
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
+
+// Singleton
+class Connection{
+    private static final Connection connection = new Connection();
+    private int connectionsCount;
+    private final Semaphore semaphore = new Semaphore(10);
+    private Connection(){
+
+    }
+    public static Connection getConnection(){
+        return connection;
+    }
+
+    public void work() throws InterruptedException {
+        semaphore.acquire();
+        try{
+            doWork();
+        }
+        finally {
+            semaphore.release();
+        }
+    }
+
+     private void doWork() throws InterruptedException {
+        synchronized (this){
+            connectionsCount++;
+            System.out.println(connectionsCount);
+        }
+        Thread.sleep(5000);
+
+        synchronized (this){
+            connectionsCount--;
+        }
     }
 }
