55351c28d3d7cef6e9b93bf85f7c2587f87ac874
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-02-11 11:31:07 +0300

    Semaphore

---
+++ src/flows10/Test10.java
@@ -0,0 +1,19 @@
+package flows10;
+
+import java.util.concurrent.Semaphore;
+
+public class Test10 {
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore semaphore = new Semaphore(3);
+
+        semaphore.acquire();
+        semaphore.acquire();
+        semaphore.acquire();
+        System.out.println("All permits have been acquired");
+        semaphore.acquire();
+        System.out.println("Can't reach here...");
+        semaphore.release();
+
+        System.out.println(semaphore.availablePermits());
+    }
+}
