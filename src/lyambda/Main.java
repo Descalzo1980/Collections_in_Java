349f5f4d96d76e4cb7e01a37bde96314ec6c4f03
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-02-21 13:26:55 +0300

    всего по немногу

--- src/lyambda/Main.java
+++ src/lyambda/Main.java
@@ -2,12 +2,12 @@ package lyambda;
 
 
 interface Executable{
-    int execute(int x);
+    int execute(int x,int y);
 }
 
 class Runner{
     public void run(Executable e){
-        int a = e.execute(10);
+        int a = e.execute(10,15);
         System.out.println(a);
     }
 }
@@ -16,13 +16,21 @@ class Runner{
 public class Main {
     public static void main(String[] args) {
         Runner runner = new Runner();
-
+        int b = 2;// scope!!!
         runner.run(new Executable() { // 2
             @Override
-            public int execute(int x) {
-                return x + 10;
+            public int execute(int x,int y) {
+                int b = 2; // scope!!!
+                return x + y;
             }
         });
-        runner.run( x -> x + 10); // 3
+        int a = 1;
+//        a = 2; òàê íåëüçÿ, final required ó ëÿìáäû íåò îáëàñòè âèäèìîñòè
+
+        runner.run( (x,y) -> {
+            int v = 1;
+            return x + y + a;
+        }); // 3
+        runner.run(Integer::sum); // 3
     }
 }
