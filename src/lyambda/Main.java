c031946add738ff563f30581a87b0ce8dfc3d339
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-02-21 11:09:05 +0300

    всего по немногу

--- src/lyambda/Main.java
+++ src/lyambda/Main.java
@@ -1,36 +1,28 @@
 package lyambda;
 
-class ExecutableImplementation implements Executable{
 
-    @Override
-    public void execute() {
-        System.out.println("Hello");
-    }
+interface Executable{
+    int execute(int x);
 }
 
 class Runner{
     public void run(Executable e){
-        e.execute();
+        int a = e.execute(10);
+        System.out.println(a);
     }
 }
 
-interface Executable{
-    void execute();
-}
 
 public class Main {
     public static void main(String[] args) {
         Runner runner = new Runner();
 
-        runner.run(new ExecutableImplementation()); // 1
-
         runner.run(new Executable() { // 2
             @Override
-            public void execute() {
-                System.out.println("Hello");
+            public int execute(int x) {
+                return x + 10;
             }
         });
-
-        runner.run( () -> System.out.println("Hello")); // 3
+        runner.run( x -> x + 10); // 3
     }
 }
